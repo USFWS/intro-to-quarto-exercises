d7a55e733c0ab384e365792cff4b89eb792852da
Cobb <mccrea_cobb@fws.gov>
2025-05-19 12:16:15 -0800

    Clean up folders, update install_packages

--- install_packages.R
+++ install_packages.R
@@ -1,7 +1,7 @@
 install_packages <- function() {
   required_packages <- c("tidyverse", "knitr", "officedown", "officer", "lubridate",
                          "readxl", "janitor", "kableExtra", "flextable", "mapview", 
-                         "sf", "gt", "purrr", "english", "maps", "quarto")
+                         "sf", "gt", "purrr", "english", "maps", "quarto", "tinytex")
   new_packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
   if(length(new_packages)) install.packages(new_packages)
   update.packages(oldPkgs = required_packages)
